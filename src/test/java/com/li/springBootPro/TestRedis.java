ba14312a030640ef567b73fce0bea2c1b01bdc16
bestRookieLee <563748645@qq.com>
2020-11-23 21:32:02 +0800

    commit 关于mybatis 一堆多自动映射结果集的测试

---
+++ src/test/java/com/li/springBootPro/TestRedis.java
@@ -0,0 +1,25 @@
+package com.li.springBootPro;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestRedis {
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    @Test
+    public void set(){
+//        for (int i = 0; i <10 ; i++) {
+//            redisTemplate.opsForValue().set("mykeyFromSrping" + i, "helloRedis" + (10*i));
+//        }
+        redisTemplate.opsForValue().set("mykeyFromSrping10", "helloRedis100");
+            System.out.println(redisTemplate.opsForValue().get("mykeyFromSrping10"));
+    }
+}
